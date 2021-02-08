d27d5a2f6ef41b069591d136d9c79becc8e0d851
Daniel Bedoya <dbbedoya@yahoo.com>
2021-02-07 22:57:47 -0700

    initial commit

---
+++ csDemoCmdLn/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ServiceLibrary;
+
+namespace vsDemoCmdLn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String importFile = "/Users/dbedoya/dev/csharp/vsDemoCmdLn/balances.csv";
+            String exportFile = "/Users/dbedoya/dev/csharp/vsDemoCmdLn/summary.csv";
+            DateTime dtdatetime = DateTime.Now;
+
+            ImportService importService = new ImportService(importFile, dtdatetime);
+            ExportService exportService = new ExportService(importFile, dtdatetime, exportFile);
+
+            int ip = importService.ProcessImport();
+
+            Console.WriteLine("ProcessImport: {0}", ip);  //Debug Info
+
+            int ep = exportService.ProcessExport();
+
+            Console.WriteLine("ProcessExport: {0}", ep);  //Debug Info
+
+            return;
+        }
+    }
+}
